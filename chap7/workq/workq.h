a8f47aaaa27fb7a645c36871773599b85b3d20b0
Mamadou Mountagha Bah <bahm1501@dinf-2014-60b.DInf.fsci.usherbrooke.ca>
2021-04-24 01:14:02 -0400

    workq added

---
+++ chap7/workq/workq.h
@@ -0,0 +1,41 @@
+#include <pthread.h>
+
+/* 
+ * Structure to keep track of work queue requests.
+ */
+
+typedef struct workq_ele_tag {
+    struct workq_ele_tag *next;
+    void *data;
+} workq_ele_t;
+
+/*
+ * Structure describing a work queue.
+ */
+
+typedef struct workq_tag {
+    pthread_mutex_t mutex;      // control access to the queue
+    pthread_cond_t cv;          // wait for work
+    pthread_attr_t attr;        // create detached threads
+    workq_ele_t *first, *last;   // work queue
+    int valid;                  // valid
+    int quit;                   // workq should quit
+    int parallelism;            // maximum threads
+    int counter;                // current threads
+    int idle;                   // number of idle threads
+    void (*engine)(void *arg);  // user engine
+} workq_t;
+
+#define WORKQ_VALID 0xdec1992
+
+/*
+ * Define work queue functions.
+ */
+
+extern int workq_init (
+    workq_t *wq,
+    int threads,         // maximum threads
+    void (*engine) (void *));   // engine routine
+
+extern int workq_destroy(workq_t *wq);
+extern int workq_add (workq_t *wq, void *data);
